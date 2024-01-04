e2e229fae96a1783ceec217ba034902a2eb41ac7
colmtuite <colmtuite@gmail.com>
2024-01-04 19:02:01 +0000

    Early menu exploration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { Button } from '@mui/base/Button';
 import { Switch } from '@mui/base/Switch';
+import { Menu } from '@mui/base/Menu';
+import { MenuItem } from '@mui/base/MenuItem';
+import { MenuButton } from '@mui/base/MenuButton';
+import { Dropdown } from '@mui/base/Dropdown';
 
 export default function Home() {
   return (
@@ -11,7 +15,13 @@ export default function Home() {
           alignItems: 'center',
           gap: 40,
         }}>
-        <Button>Create Repository</Button>
+        <Button
+          slotProps={{
+            root: { className: 'Button' },
+          }}
+        >
+          Button
+        </Button>
         <Switch
           slotProps={{
             root: { className: 'SwitchRoot' },
@@ -20,6 +30,47 @@ export default function Home() {
             track: { className: 'SwitchTrack' },
           }}
         />
+        <Dropdown>
+          <MenuButton className="DropdownMenu-trigger">Dropdown</MenuButton>
+
+          <Menu
+            className="CustomMenuIntroduction"
+            slotProps={{
+              listbox: { className: 'DropdownMenu-listbox' },
+            }}
+          >
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Profile
+            </MenuItem>
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Language settings
+            </MenuItem>
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Log out
+            </MenuItem>
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Profile
+            </MenuItem>
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Language settings
+            </MenuItem>
+            <MenuItem
+              className="DropdownMenu-item"
+            >
+              Log out
+            </MenuItem>
+          </Menu>
+        </Dropdown>
       </div>
     </main>
   )
