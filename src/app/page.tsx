e18931f8dbd0fd7cf1c000c671d817204f2afc0d
colmtuite <colmtuite@gmail.com>
2024-01-04 14:43:35 +0000

    Switch component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import { Button } from '@mui/base/Button';
+import { Switch } from '@mui/base/Switch';
 
 export default function Home() {
   return (
     <main>
-      <Button className="btn">Create Repository</Button>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 40,
+        }}>
+        <Button>Create Repository</Button>
+        <Switch
+          slotProps={{
+            root: { className: 'SwitchRoot' },
+            input: { className: 'SwitchInput' },
+            thumb: { className: 'SwitchThumb' },
+            track: { className: 'SwitchTrack' },
+          }}
+        />
+      </div>
     </main>
   )
 }
